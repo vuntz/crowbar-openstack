052e85d2dd67470620ccaf8975c95dc38f9c42c3
Vincent Untz <vuntz@suse.com>
2015-10-01 17:41:49 +0200

    Fixes for memcached

--- chef/cookbooks/memcached/attributes/default.rb
+++ chef/cookbooks/memcached/attributes/default.rb
@@ -6,7 +6,7 @@ default[:memcached][:daemonize] = true
 case node[:platform_family]
 when "suse"
   default[:memcached][:user] = "memcached"
-  default[:memcached][:daemonize] = false if node[:platform_version].to_f >= 12.0 || node[:platform] == "opensuse"
+  default[:memcached][:daemonize] = false unless (node[:platform] == "suse" && node[:platform_version].to_f < 12.0)
 else
   default[:memcached][:user] = "nobody"
 end
